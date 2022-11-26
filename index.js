510bdea5c74243d21d1e20b7b98c761f3f953a5d
mdyiaz <mdyiaz@gmail.com>
2022-11-26 12:23:17 +0600

    create jwt Token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -166,6 +167,33 @@ async function run (){
                 })
 
             // post users data_____________________________________________________________________________________________
+
+
+// jwt Token________________________________________________________________________________________
+
+
+            app.get('/jwt', async(req, res) => {
+                const email = req.query.email;
+                const query = {email: email};
+                const user = await usersCollection.findOne(query);
+                if(user){
+                     const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
+                     return res.send({accessToken: token });
+                }
+               
+                res.status(403).send({accessToken: ''})
+            }) 
+
+
+// jwt Token________________________________________________________________________________________
+
+
+
+
+
+
+
+
 
 
         }
